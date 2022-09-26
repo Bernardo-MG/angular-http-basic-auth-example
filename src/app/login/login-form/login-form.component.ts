f213484d5014831b878c2502b16ab71dd84752c0
Bernardo <programming@bernardomg.com>
2022-09-26 22:09:27 +0200

    Better handling of login form status

--- src/app/login/login-form/login-form.component.ts
+++ src/app/login/login-form/login-form.component.ts
@@ -10,59 +10,63 @@ import { AuthenticationService } from '@app/authentication/service/authenticatio
 })
 export class LoginFormComponent implements OnInit {
 
-  loginForm: FormGroup;
-  loading = false;
-  submitted = false;
-  returnUrl: string = '';
-  error = '';
+  public form = this.formBuilder.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  public loading = false;
+
+  public submitted = false;
+
+  public error = '';
+
+  private returnUrl: string = '';
 
   constructor(
     private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
     private authenticationService: AuthenticationService
-  ) {
+  ) { }
+
+  ngOnInit() {
     // redirect to home if already logged in
     if (this.authenticationService.getUser().logged) {
       this.router.navigate(['/']);
     }
-
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
-  }
-
-  ngOnInit() {
     // get return url from route parameters or default to '/'
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  // convenience getter for easy access to form fields
-  get f() { return this.loginForm.controls; }
-
-  onSubmit() {
+  public onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
-
-    this.loading = true;
-    this.authenticationService.login(this.f['username'].value, this.f['password'].value)
-      .subscribe({
-        next: user => {
-          this.loading = false;
-          if (user.logged) {
-            this.router.navigate([this.returnUrl]);
+    if (this.form.valid) {
+      this.loading = true;
+      this.authenticationService.login(this.form.value.username, this.form.value.password)
+        .subscribe({
+          next: user => {
+            this.loading = false;
+            if (user.logged) {
+              this.router.navigate([this.returnUrl]);
+            }
+          },
+          error: error => {
+            this.error = error;
+            this.loading = false;
           }
-        },
-        error: error => {
-          this.error = error;
-          this.loading = false;
-        }
-      });
+        });
+    }
+  }
+
+  public isFormInvalid(): boolean {
+    return this.form.invalid && (this.form.dirty || this.form.touched);
+  }
+
+  public canLogin(): boolean {
+    return this.form.valid;
   }
 
 }
