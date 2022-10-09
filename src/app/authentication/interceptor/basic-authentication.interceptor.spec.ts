efe97bb05dd7c7c8534c361a4db45a52e67e849a
Bernardo <programming@bernardomg.com>
2022-10-09 20:41:34 +0200

    Created login service

--- src/app/authentication/interceptor/basic-authentication.interceptor.spec.ts
+++ src/app/authentication/interceptor/basic-authentication.interceptor.spec.ts
@@ -1,13 +1,9 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BasicAuthenticationInterceptor } from './basic-authentication.interceptor';
 
 describe('BasicAuthenticationInterceptor', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [
-      HttpClientTestingModule
-    ],
     providers: [
       BasicAuthenticationInterceptor
     ]
--- src/app/authentication/interceptor/basic-authentication.interceptor.ts
+++ src/app/authentication/interceptor/basic-authentication.interceptor.ts
@@ -2,7 +2,7 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Injectable } from '@angular/core';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
-import { AuthenticationService } from '../service/authentication.service';
+import { AuthenticationContainer } from '../service/authentication-container.service';
 
 /**
  * Basic HTTP authentication interceptor. Adds the basic authentication token to all request to
@@ -16,7 +16,7 @@ export class BasicAuthenticationInterceptor implements HttpInterceptor {
   private tokenHeaderIdentifier = 'Basic'
 
   constructor(
-    private authenticationService: AuthenticationService
+    private authenticationContainer: AuthenticationContainer
   ) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -28,8 +28,8 @@ export class BasicAuthenticationInterceptor implements HttpInterceptor {
       // It is a request to our API
 
       // Acquire the current user token
-      const logged = this.authenticationService.getUser().logged;
-      const token = this.authenticationService.getUser().token;
+      const logged = this.authenticationContainer.getLoginDetails().logged;
+      const token = this.authenticationContainer.getLoginDetails().token;
 
       if ((logged) && (token)) {
         // Has token
